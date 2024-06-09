1013f261e050b85c79a7065808c5d6eee87944c2
Facundo Martin <facundomartin98@gmail.com>
2024-06-08 17:53:08 -0700

    feat: add image upload with s3 bucket

--- src/app/items/create/actions.ts
+++ src/app/items/create/actions.ts
@@ -1,10 +1,16 @@
 "use server";
 
-import { auth } from "@/auth";
-import { database } from "@/db/database";
-import { items } from "@/db/schema";
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+import { auth } from "@/auth";
+import { items } from "@/db/schema";
+import { database } from "@/db/database";
+import { getSignedUrlForS3Object } from "@/lib/s3";
+
+export async function createUploadUrlAction(key: string) {
+  return await getSignedUrlForS3Object(key);
+}
 
 export async function createItemAction(formData: FormData) {
   const session = await auth();
@@ -17,6 +23,9 @@ export async function createItemAction(formData: FormData) {
 
   const startingPrice = formData.get("startingPrice") as string;
   const priceAsCents = Math.floor(parseFloat(startingPrice) * 100);
+
+  const file = formData.get("file") as File;
+  console.log(file);
 
   await database.insert(items).values({
     name: formData.get("name") as string,
--- src/app/items/create/page.tsx
+++ src/app/items/create/page.tsx
@@ -1,19 +1,36 @@
+"use client";
+
 import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { database } from "@/db/database";
-import { createItemAction } from "./actions";
+import { createItemAction, createUploadUrlAction } from "./actions";
 
-export default async function Page() {
-  const session = await auth();
-  const allItems = await database.query.items.findMany();
-
+export default function Page() {
   return (
     <main className="container mx-auto py-12 space-y-8">
       <h1 className="text-4xl font-bold">Post an Item</h1>
       <form
         className="border p-8 rounded-xl space-y-4 max-w-lg flex flex-col"
-        action={createItemAction}
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          const form = e.currentTarget as HTMLFormElement;
+          const formData = new FormData(form);
+          const file = formData.get("file") as File;
+
+          const uploadUrl = await createUploadUrlAction(file.name);
+
+          const uploadFormData = new FormData();
+          uploadFormData.append("file", file);
+
+          await fetch(uploadUrl, {
+            method: "PUT",
+            body: uploadFormData,
+          });
+
+          await createItemAction(formData);
+        }}
       >
         <Input required name="name" type="text" placeholder="Name your item" />
         <Input
@@ -24,6 +41,7 @@ export default async function Page() {
           step="0.01"
           placeholder="Where to start your auction at"
         />
+        <Input required name="file" type="file" placeholder="Name your item" />
         <Button className="self-end" type="submit">
           Post item
         </Button>
